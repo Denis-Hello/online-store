68ae8ac576cfb6c6bd8746927d3eda4633e06951
Denis <super.stavro2014@yandex.ru>
2024-11-18 21:36:09 +0300

    сделал верстку хедера

---
+++ src/Controller/test.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class test extends AbstractController
+{
+    #[Route('/test')]
+    public function go():Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
