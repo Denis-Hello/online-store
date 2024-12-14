0cd814f50c0ef7b60d128a42922ffd2a319c2ffd
Denis <super.stavro2014@yandex.ru>
2024-12-14 17:58:16 +0300

    доделал раздел новинок

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -5,11 +5,21 @@ import '../styles/slider.css'
 
 
 
-const swiper = new Swiper('.swiper', {
+const hero_swiper = new Swiper(".hero__slider", {
     // autoplay: {
     //     delay: 3000,
     // },
     effect: 'fade',
+});
+
+const new_swiper = new Swiper(".new__slider", {
+    spaceBetween: 40,
+    slidesPerView: 4,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
 
 
